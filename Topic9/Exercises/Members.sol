0e64de20060bebba04dce3fef643c0b4593121db
ArnaudovSt <telerikacademy2016@gmail.com>
2018-03-14 00:48:40 +0200

    Added Members exercise

---
+++ Topic9/Exercises/Members.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.21;
+
+contract Members {
+    mapping(address => uint256) public members; // key -> adr, value -> joinedAt
+    
+    function Members(address[] addresses) public {
+        for(uint i = 0; i < addresses.length; i++) {
+            members[addresses[i]] = now;
+        }
+    }
+}
